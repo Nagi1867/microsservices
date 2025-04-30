7248a3e5f82069f77a69ec1b68a25c6d346035d5
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-30 16:58:13 -0300

    implementando métodos

---
+++ src/main/java/com/example/microsservices/MathController.java
@@ -0,0 +1,27 @@
+package com.example.microsservices;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = "/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        return null;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        return false;
+    }
+}
