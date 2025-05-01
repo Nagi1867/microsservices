496d07f4400943b7fe4acea6caae04b7f3ca7005
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-05-01 12:17:27 -0300

    find all

--- src/main/java/com/example/microsservices/controllers/PersonController.java
+++ src/main/java/com/example/microsservices/controllers/PersonController.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonServices service;
+
+    @GetMapping
+    public List<Person> findById() {
+        return service.findAll();
+    }
 
     @GetMapping(value = "/{id}")
     public Person findById(@PathVariable(value = "id") String id) throws Exception {
--- src/main/java/com/example/microsservices/services/PersonServices.java
+++ src/main/java/com/example/microsservices/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.example.microsservices.services;
 import com.example.microsservices.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,16 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        logger.info("Finding all person!");
+        List<Person> persons = new ArrayList<>();
+        for (int i=0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one person!");
         Person person = new Person();
@@ -18,6 +30,16 @@ public class PersonServices {
         person.setFirstName("Giusepi");
         person.setLastName("Costa");
         person.setAddress("Sp");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brasil " + i);
         person.setGender("Male");
         return person;
     }
