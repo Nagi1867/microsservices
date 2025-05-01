2698177fadcb43e00ca81677d3d6872e7dc1c069
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-05-01 16:02:10 -0300

    repository

---
+++ src/main/java/com/example/microsservices/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.example.microsservices.repositories;
+
+import com.example.microsservices.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
